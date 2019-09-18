867847af48812af5766b1745fdf295cbb41cff81
BuildTools <unconfigured@null.spigotmc.org>
2019-09-17 21:07:36 -0300

    Add project files.

---
+++ Videoman/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace Videoman
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.selectFile = new System.Windows.Forms.Button();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
+            this.bufferSize = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bufferSize)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 78);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Current File:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Enabled = false;
+            this.textBox1.Location = new System.Drawing.Point(82, 75);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(527, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // selectFile
+            // 
+            this.selectFile.Location = new System.Drawing.Point(15, 12);
+            this.selectFile.Name = "selectFile";
+            this.selectFile.Size = new System.Drawing.Size(194, 47);
+            this.selectFile.TabIndex = 2;
+            this.selectFile.Text = "SELECT FILE";
+            this.selectFile.UseVisualStyleBackColor = true;
+            this.selectFile.Click += new System.EventHandler(this.SelectFile_Click);
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.FileName = "openFileDialog1";
+            this.openFileDialog.Title = "Select a File";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 101);
+            this.progressBar1.Maximum = 10000;
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(597, 23);
+            this.progressBar1.Step = 1;
+            this.progressBar1.TabIndex = 4;
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(12, 131);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(597, 340);
+            this.axWindowsMediaPlayer1.TabIndex = 5;
+            // 
+            // bufferSize
+            // 
+            this.bufferSize.Location = new System.Drawing.Point(318, 12);
+            this.bufferSize.Name = "bufferSize";
+            this.bufferSize.Size = new System.Drawing.Size(120, 20);
+            this.bufferSize.TabIndex = 6;
+            this.bufferSize.Value = new decimal(new int[] {
+            50,
+            0,
+            0,
+            0});
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(224, 14);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(88, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Buffer Size (MiB):";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(621, 483);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.bufferSize);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.selectFile);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bufferSize)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button selectFile;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
+        private System.Windows.Forms.NumericUpDown bufferSize;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Videoman/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Videoman
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private String file;
+
+        private void SelectFile_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                String odf = openFileDialog.FileName;
+                if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    String sfd = saveFileDialog1.FileName;
+                    encrypt(odf, (int)bufferSize.Value * 1024 * 1024, sfd);
+                }
+                
+            }
+        }
+
+        private void encrypt(String path, int chunksize, String output)
+        {
+            using (FileStream fw = new FileStream(output, FileMode.Create, FileAccess.Write))
+            using (FileStream fs = new FileStream(path, FileMode.Open, FileAccess.Read))
+            {
+                // Get file full size in bytes
+                long fullsize = fs.Length;
+
+                // Get the number of steps used in the progressbar
+                long maximum = fullsize / chunksize;
+                progressBar1.Maximum = (int) maximum + 1;
+                int bytesRead;
+                var buffer = new byte[chunksize];
+                axWindowsMediaPlayer1.URL = output;
+                while ((bytesRead = fs.Read(buffer, 0, chunksize)) > 0)
+                {
+                    foreach (byte b in buffer)
+                    {
+                        fw.WriteByte((byte)(b ^ 0x7c));
+                        axWindowsMediaPlayer1.settings.autoStart = true;
+                    }
+                    progressBar1.PerformStep();
+                }
+            }
+            progressBar1.Value = 0;
+            
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            progressBar1.PerformStep();
+        }
+    }
+}
